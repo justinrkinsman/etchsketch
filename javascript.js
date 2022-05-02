f34c238f08bbcb46d05e0060162b246d6b401cbf
Justin <justinrkinsman@gmail.com>
2022-05-02 15:50:00 -0300

    Create grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-let container = document.getElementById('container');
-let cell = document.createElement('div');
-cell.innerHTML = 'TEXT';
-container.appendChild(cell);
+for (let i = 0; i < 30; i++) {
+    for (let j = 0; j < 30; j++){
+        let grid = document.createElement('div');
+        grid.className = 'grid';
+        document.body.appendChild(grid);
+        grid.innerText = 'Cell'
+    }
+    let br = document.createElement('br');
+    document.body.appendChild(br);
+}
