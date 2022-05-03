d00255c517408275cc858874e7d31e487ee31893
Justin <justinrkinsman@gmail.com>
2022-05-03 15:00:13 -0300

    Test functionality

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
+//let xval = prompt()
+
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++){
         let grid = document.createElement('div');
         grid.className = 'grid';
         container.appendChild(grid);
         }
+        document.getElementById('container').style.gridTemplateColumns = `repeat(10, 1fr)`;
     }
 
-
-function changeGrid() {
-    const gridSize = document.querySelector('#newGrid');
+//function newGridSize() {
+    document.querySelector('#newGrid');
     newGrid.addEventListener('click', function click(event) {
-        prompt('Enter new grid size (Max: 100)')
+        const gridSize = prompt('Enter new grid size (Max: 100)')
+        return gridSize
         });
-    }
-
-document.getElementById('container').style.gridTemplateColumns = `repeat(10, 1fr)`;
+  //}
 
 const cell = document.querySelectorAll('.grid');
 for (let i = 0; i < cell.length; i++) {
@@ -23,4 +24,4 @@ for (let i = 0; i < cell.length; i++) {
     });
 }
 
-changeGrid()
+//newGridSize()
