dd8cbf7b103595f488fbe7ae89fea8ea32e2d65a
Justin <justinrkinsman@gmail.com>
2022-05-03 10:07:36 -0300

    Add functionality to etch-a-sketch

--- javascript.js
+++ javascript.js
@@ -3,7 +3,21 @@ for (let i = 0; i < 16; i++) {
         let grid = document.createElement('div');
         grid.className = 'grid';
         document.body.appendChild(grid);
+        grid.addEventListener('onmouseover', () => {
+        })
     }
     let br = document.createElement('br');
     document.body.appendChild(br);
 }
+
+const cell = document.querySelectorAll('.grid');
+for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('mouseover', function onmouseover(event) {
+        event.target.style.backgroundColor = 'black';
+    });
+}
+
+const newGrid = document.querySelector('#newGrid');
+newGrid.addEventListener('click', function click(event) {
+    event.target.style.backgroundColor = 'blue';
+    });
